17f976a843b03198b52d201caac0078081a55f44
gurle <gurle@192.168.2.10>
2020-05-29 22:08:58 -0400

    first commit

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author gurle
+ */
+public class Student {
+    
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+
+}
+
